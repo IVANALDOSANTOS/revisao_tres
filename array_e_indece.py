19fa6afb9bb91c29930d4e10d6b65c2bc46ec4b5
ivanaldo da silva santos <ivanaldo594@gmail.com>
2024-04-04 20:24:42 -0300

    primeiro commit

---
+++ array_e_indece.py
@@ -0,0 +1,9 @@
+def imprimir_nomes():
+
+    nomes = ["João ", "Maria", "Pedro", "José"]
+    print("1 " , nomes[0])
+    print("2 " , nomes[1])
+    print("3 " ,nomes[2])
+    print("4 " , nomes[3])
+
+imprimir_nomes()    
