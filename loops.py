1866914020e82f4fc8160ed0c2b6d0f5e922267b
ivanaldo da silva santos <ivanaldo594@gmail.com>
2024-04-04 21:49:34 -0300

    função de contagem

--- loops.py
+++ loops.py
@@ -1,3 +1,5 @@
+#Crie uma função que imprime uma contagem de 0 a 2
+
 def contar_ate_vinte():
     
     contador = 0
@@ -6,3 +8,16 @@ def contar_ate_vinte():
         contador = contador + 1
 
 contar_ate_vinte()
+
+#*****************************
+#6. Crie uma função que imprime uma contagem de 0 até o número que o usuário digitou
+
+def print_count(n):
+    for i in range(n + 1):
+        print(i)
+
+# Obter entrada do usuário
+num = int(input("Digite um número: "))
+
+# Chamar a função com a entrada do usuário
+print_count(num)
