9d6908499f37b4b962343efd14e5ab36cc38afb9
ivanaldo da silva santos <ivanaldo594@gmail.com>
2024-04-04 21:51:08 -0300

    tabuada do dois

--- loops.py
+++ loops.py
@@ -21,3 +21,23 @@ num = int(input("Digite um número: "))
 
 # Chamar a função com a entrada do usuário
 print_count(num)
+
+#***********************************
+
+#7. Crie uma função que imprime a tabuada de adição do número
+def print_addition_table(n):
+    for i in range(1, 11):
+        print(f"{n} + {i} = {n + i}")
+
+# Obter entrada do usuário
+num = int(input("Digite um número: "))
+
+# Chamar a função com a entrada do usuário
+print_addition_table(num)
+
+def print_addition_table(n):
+    for i in range(1, 11):
+        print(f"{n} + {i} = {n + i}")
+
+# Chamar a função com o número 2 como argumento
+print_addition_table(2)
